0ce2454871604ba2e6a2da7b3cc0595654eb4c18
sannchu <alejandrosanzdev@gmail.com>
2026-09-21 20:16:38 +0200

    Actualiza formulario y lectura de tecnologias

--- jakartalogin/src/main/java/es/daw/jakartalogin/AltaServlet.java
+++ jakartalogin/src/main/java/es/daw/jakartalogin/AltaServlet.java
@@ -1,6 +1,9 @@
 package es.daw.jakartalogin;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import jakarta.servlet.ServletException;
@@ -11,13 +14,23 @@ import jakarta.servlet.annotation.*;
 @WebServlet("/alta")
 public class AltaServlet extends HttpServlet {
 
-    private static final Logger logger = Logger.getLogger(AltaServlet.class.getName());
+private static final Logger LOGGER = Logger.getLogger(AltaServlet.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try {
+            List<String> tecnologias = leerFichero("/WEB-INF/datos/tecnologias.txt");
+            LOGGER.info(tecnologias.toString());
+
+            request.getRequestDispatcher("/formulario.jsp").forward(request, response);
 
 
+
+        }catch ( IOException e){
+            // PENDIENTE: enviar a una pagina jsp de error el mensaje de error...
+            LOGGER.severe(e.getMessage());
+        }
 
     }
 
@@ -28,5 +41,25 @@ public class AltaServlet extends HttpServlet {
 
 
     }
+
+    private List<String> leerFichero(String pathFile) throws IOException{
+        List<String> lista = new ArrayList<>();
+
+        InputStream is = getServletContext().getResourceAsStream(pathFile);
+
+        if ( is ==null)
+            throw new IOException("No se encuentra: "+ pathFile);
+
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(is,StandardCharsets.UTF_8))){
+            String linea;
+            while((linea = br.readLine()) != null){
+                if (!linea.isBlank())
+                    lista.add(linea.trim());
+            }
+        }
+        return lista;
+
+    }
+
 
 }
--- jakartalogin/src/main/webapp/formulario.jsp
+++ jakartalogin/src/main/webapp/formulario.jsp
@@ -31,7 +31,15 @@
       <label for="tecnologia">Tecnología con la que más te gustaría trabajar</label>
       <select id="tecnologia" name="tecnologia">
 
-            <option value=" "> </option>
+            <%
+                List<String> tecnologias = (List<String>)request.getAttribute("tecnologias");
+                for (String t: tecnologias){
+
+                }
+            %>
+            <option value="<%=t%>"><%=t%></option>
+
+            <% } %>
       </select>
 
       <label for="nivel">Tu nivel actual</label>
