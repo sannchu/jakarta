5501c2a83fcdcf26d4aa32b758a8189231d79614
Alejandro Sanz <108949263+sannchu@users.noreply.github.com>
2026-09-25 18:56:50 +0200

    Ajusta la excepción de fichero no encontrado

--- jakartalogin/src/main/java/es/daw/jakartalogin/exception/TxtNoEncontradoException.java
+++ jakartalogin/src/main/java/es/daw/jakartalogin/exception/TxtNoEncontradoException.java
@@ -1,8 +1,8 @@
+
 package es.daw.jakartalogin.exception;
 
 public class TxtNoEncontradoException extends Exception {
 
-    private static final long serialVersionUID = 1L;
 
     public TxtNoEncontradoException(String message) {
         super(message);
